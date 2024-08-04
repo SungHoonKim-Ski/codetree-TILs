7b179c4550dd3cc844698bce5f0a11ae7549360b
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2024-08-04 15:31:05 +0900

    Title: 냉방 시스템, Time: 317ms, Memory: 21MB, Status: Passed - CodeTree

--- 240804/냉방 시스템/cooling-system.java
+++ 240804/냉방 시스템/cooling-system.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
@@ -18,19 +17,6 @@ public class Main {
     static int[] dy = {0, -1, 0, 1};
     static int[] dx = {-1, 0, 1, 0};
 
-    static int[][] ax = {
-            {-1, 0, 10, 1}, // 왼쪽 위, 왼쪽, x, 왼쪽 아래
-            {-1, -1, -1, 10}, // 위 왼쪽, 위, 위 오른쪽, x
-            {10, -1, 0, 1}, // x, 오른쪽 위, 오른쪽, 오른쪽 아래
-            {1, 10, 1, 1} // 아래 왼쪽, x, 아래, 아래 오른쪽
-    };
-    static int[][] ay = {
-            {-1, -1, 10, -1}, // 왼쪽 위, 왼쪽, 왼쪽 아래
-            {-1, 0, 1, 10}, // 위 왼쪽, 위, 위 오른쪽
-            {10, 1, 1, 1}, // x, 오른쪽 위, 오른쪽, 오른쪽 아래
-            {-1, 10, 0, 1} // 아래 왼쪽, x, 아래, 아래 오른쪽
-    };
-
     public static void main(String[] args) throws IOException {
 
         input();
@@ -42,7 +28,7 @@ public class Main {
             time++;
             if (isCool()) break;
         }
-//        print();
+
         if (time == 100) time = -1;
         System.out.println(time);
     }
@@ -61,7 +47,6 @@ public class Main {
 
             int nextX = airconX + dx[airconDir];
             int nextY = airconY + dy[airconDir];
-//            curCool[nextX][nextY] += 5;
 
             que.add(new int[] {nextY, nextX, 5});
 
@@ -76,12 +61,8 @@ public class Main {
                 if (cur[2] == 1) continue;
 
                 nextPos.clear();
-
+                // 왼쪽, 위, 오른쪽, 아래
                 int[] nextDir = {(airconDir + 1) % 4, (airconDir + 4 - 1) % 4};
-//                nextY = curY + dy[airconDir];
-//                nextX = curX + dx[airconDir];
-
-//                if (nextX != -1 && nextY != -1 && nextX != n && nextY != n)
                 nextPos.add(new int[] {curY, curX, cur[2]});
 
                 for (int dir : nextDir) {
@@ -120,25 +101,17 @@ public class Main {
                     if (ny == -1 || ny == n) continue;
                     if (nx == -1 || nx == n) continue;
 
-                    if (wall[(dir + 4 - 2) % 4][i][j]) continue;
+                    if (wall[dir][i][j]) continue;
 
                     int diff = (curCool[i][j] - curCool[ny][nx]) / 4;
                     if (diff == 0) continue;
 
-//                    if (diff > 0) { // 현재 위치가 냉기를 뺏기는 경우
-                        nextCool[i][j] -= diff;
-                        nextCool[ny][nx] += diff;
-//                    } else { // 현재 위치가 냉기를 받는 경우
-//                        nextCool[i][j] += diff;
-//                        nextCool[ny][nx] -= diff;
-//                    }
+                    nextCool[i][j] -= diff;
+                    nextCool[ny][nx] += diff;
                 }
 
             }
         }
-//        for (int i = 0; i < n; i++) {
-//            System.out.println(Arrays.toString(nextCool[i]));
-//        }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -213,12 +186,11 @@ public class Main {
             int dir = Integer.parseInt(st.nextToken());
             if (dir == 0) { // 위쪽에 벽
                 wall[1][y][x] = true; // 위 방향 기준
-                if (y - 1 != -1) wall[3][y - 1][x] = true; // 아래 방향 기준
+                if (y - 1 != n) wall[3][y - 1][x] = true; // 아래 방향 기준
             } else { // 왼쪽에 벽
                 wall[0][y][x] = true;
-                if (x - 1 != -1) wall[2][y][x - 1] = true; // 오른쪽 방향 기준
+                if (x - 1 != n) wall[2][y][x - 1] = true; // 오른쪽 방향 기준
             }
         }
     }
-
 }
