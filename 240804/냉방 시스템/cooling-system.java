9c85de13573820223beceefd999dc86a91955f51
성훈 <57925467+SungHoonKim-Ski@users.noreply.github.com>
2024-08-04 15:55:37 +0900

    Title: 냉방 시스템, Time: 294ms, Memory: 17MB, Status: Passed - CodeTree

--- 240804/냉방 시스템/cooling-system.java
+++ 240804/냉방 시스템/cooling-system.java
@@ -1,191 +1,254 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int OFFICE = 1;
+    public static final int DIR_NUM = 4;
+    public static final int MAX_M = 400;
+    public static final int MAX_N = 20;
     
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-
-    static int n, m, k, time;
-
-    static int[][] curCool, nextCool;
-    static boolean[][][] wall;
-    static ArrayList<int[]> offices, aircons;
-
-    static ArrayList<int[]> nextPos;
-    static Deque<int[]> que;
-
-
-    static boolean[][] visit;
-    // 왼쪽, 위, 오른쪽, 아래
-    static int[] dy = {0, -1, 0, 1};
-    static int[] dx = {-1, 0, 1, 0};
-
-    public static void main(String[] args) throws IOException {
-
-        input();
-
-        while (time != 100) {
-            makeCool();
-            mixCool();
-            makeHot();
-            time++;
-            if (isCool()) break;
-        }
-
-        if (time == 100) time = -1;
-        System.out.println(time);
+    public static int n, m, k;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    
+    // 각 위치의 시원함의 정도를 관리합니다.
+    // 처음에는 전부 0입니다.
+    public static int[][] coolness = new int[MAX_N][MAX_N];
+    
+    // 시원함을 mix할 때
+    // 동시에 일어나는 처리를
+    // 편하게 하기 위해 사용될 배열입니다.
+    public static int[][] temp = new int[MAX_N][MAX_N];
+    
+    // dx, dy 순서를 상좌우하로 설정합니다.
+    // 입력으로 주어지는 숫자에 맞추며,
+    // 4에서 현재 방향을 뺏을 때, 반대 방향이 나오도록 설정한 것입니다.
+    public static int[] dx = new int[]{-1,  0, 0, 1};
+    public static int[] dy = new int[]{ 0, -1, 1, 0};
+    
+    // 현재 위치 (x, y)에서 해당 방향으로
+    // 이동한다 했을 때 벽이 있는지를 나타냅니다.
+    public static boolean[][][] block = new boolean[MAX_N][MAX_N][DIR_NUM];
+    
+    // 시원함을 전파할 시
+    // 한 에어컨에 대해
+    // 겹쳐서 퍼지는 경우를 막기 위해
+    // visited 배열을 사용합니다.
+    public static boolean[][] visited = new boolean[MAX_N][MAX_N];
+    
+    // 현재까지 흐른 시간(분)을 나타냅니다.
+    public static int elapsedTime;
+    
+    // (x, y)가 격자 내에 있는지를 판단합니다.
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
     }
-
-    static void makeCool() {
-
-        for (int[] aircon: aircons) {
-
-            visit = new boolean[n][n];
-            int airconY = aircon[0];
-            int airconX = aircon[1];
-            int airconDir = aircon[2];
-
-            int nextX = airconX + dx[airconDir];
-            int nextY = airconY + dy[airconDir];
-
-            que.add(new int[] {nextY, nextX, 5});
-
-            while (!que.isEmpty()) {
-
-                int[] cur = que.poll();
-                int curY = cur[0];
-                int curX = cur[1];
-
-                curCool[curY][curX] += cur[2];
-
-                if (cur[2] == 1) continue;
-
-                nextPos.clear();
-                // 왼쪽, 위, 오른쪽, 아래
-                int[] nextDir = {(airconDir + 1) % 4, (airconDir + 4 - 1) % 4};
-                nextPos.add(new int[] {curY, curX, cur[2]});
-
-                for (int dir : nextDir) {
-
-                    if (wall[dir][curY][curX]) continue;
-                    nextY = curY + dy[dir];
-                    nextX = curX + dx[dir];
-                    if (nextX == -1 || nextY == -1 || nextX == n || nextY == n) continue;
-
-                    nextPos.add(new int[] {nextY, nextX, cur[2]});
-                }
-
-                for (int[] next : nextPos) {
-                    nextY = next[0] + dy[airconDir];
-                    nextX = next[1] + dx[airconDir];
-                    if (nextX == -1 || nextY == -1 || nextX == n || nextY == n) continue;
-                    if (wall[(airconDir + 2) % 4][nextY][nextX]) continue;
-                    if (visit[nextY][nextX]) continue;
-                    visit[nextY][nextX] = true;
-                    que.add(new int[] {nextY, nextX, next[2] - 1});
-
-                }
+    
+    // (dx, dy) 값으로부터
+    // moveDir값을 추출해냅니다.
+    public static int revDir(int xDiff, int yDiff) {
+        for(int i = 0; i < 4; i++)
+            if(dx[i] == xDiff && dy[i] == yDiff)
+                return i;
+    
+        return -1;
+    }
+    
+    // (x, y)위치에서 moveDir 방향으로
+    // power 만큼의 시원함을 만들어줍니다.
+    // 이는 그 다음 칸에게도 영향을 끼칩니다.
+    public static void spread(int x, int y, int moveDir, int power) {
+        // power가 0이 되면 전파를 멈춥니다.
+        if(power == 0)
+            return;
+    
+        // 방문 체크를 하고, 해당 위치에 power를 더해줍니다.
+        visited[x][y] = true;
+        coolness[x][y] += power;
+    
+        // Case 1. 직진하여 전파되는 경우입니다.
+        int nx = x + dx[moveDir];
+        int ny = y + dy[moveDir];
+        if(inRange(nx, ny) && !visited[nx][ny] && !block[x][y][moveDir])
+            spread(nx, ny, moveDir, power - 1);
+        
+        // Case 2. 대각선 방향으로 전파되는 경우입니다.
+        if(dx[moveDir] == 0) {
+            int[] nxs = new int[]{x + 1, x - 1};
+            for(int i = 0; i < 2; i++) {
+                nx = nxs[i];
+                ny = y + dy[moveDir];
+                // 꺾여 들어가는 곳에 전부 벽이 없는 경우에만 전파가 가능합니다.
+                if(inRange(nx, ny) && !visited[nx][ny] && !block[x][y][revDir(nx - x, 0)] && !block[nx][y][moveDir])
+                    spread(nx, ny, moveDir, power - 1);
+            }
+        }
+        else {
+            int[] nys = new int[]{y + 1, y - 1};
+            for(int i = 0; i < 2; i++) {
+                nx = x + dx[moveDir];
+                ny = nys[i];
+                // 꺾여 들어가는 곳에 전부 벽이 없는 경우에만 전파가 가능합니다.
+                if(inRange(nx, ny) && !visited[nx][ny] && !block[x][y][revDir(0, ny - y)] && !block[x][ny][moveDir])
+                    spread(nx, ny, moveDir, power - 1);
             }
         }
     }
-
-    static void mixCool() {
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-
-                for (int dir = 2; dir < 4; dir++) {
-
-                    int ny = i + dy[dir];
-                    int nx = j + dx[dir];
-                    if (ny == -1 || ny == n) continue;
-                    if (nx == -1 || nx == n) continue;
-
-                    if (wall[dir][i][j]) continue;
-
-                    int diff = (curCool[i][j] - curCool[ny][nx]) / 4;
-                    if (diff == 0) continue;
-
-                    nextCool[i][j] -= diff;
-                    nextCool[ny][nx] += diff;
+    
+    public static void clearVisited() {
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                visited[i][j] = false;
+    }
+    
+    // 에어컨에서 시원함을 발산합니다.
+    public static void blow() {
+        // 각 에어컨에 대해
+        // 시원함을 발산합니다.
+        for(int x = 0; x < n; x++)
+            for(int y = 0; y < n; y++)
+                // 에어컨에 대해
+                // 해당 방향으로 시원함을
+                // 만들어줍니다.
+                if(grid[x][y] >= 2) {
+                    int moveDir = (grid[x][y] <= 3) ? (3 - grid[x][y]) 
+                                                     : (grid[x][y] - 2);
+                    int nx = x + dx[moveDir];
+                    int ny = y + dy[moveDir];
+    
+                    // 전파 전에 visited 값을 초기화해줍니다.
+                    clearVisited();
+                    // 세기 5에서 시작하여 계속 전파합니다.
+                    spread(nx, ny, moveDir, 5);
                 }
-
+    }
+    
+    // (x, y) 위치는
+    // mix된 이후 시원함이
+    // 얼마가 되는지를 계산해줍니다.
+    public static int getMixedCoolness(int x, int y) {
+        int remainingC = coolness[x][y];
+        for(int i = 0; i < DIR_NUM; i++) {
+            int nx = x + dx[i], ny = y + dy[i];
+            // 사이에 벽이 존재하지 않는 경우에만 mix가 일어납니다.
+            if(inRange(nx, ny) && !block[x][y][i]) {
+                // 현재의 시원함이 더 크다면, 그 차이를 4로 나눈 값 만큼 빠져나갑니다.
+                if(coolness[x][y] > coolness[nx][ny])
+                    remainingC -= (coolness[x][y] - coolness[nx][ny]) / 4;
+                // 그렇지 않다면, 반대로 그 차이를 4로 나눈 값만큼 받아오게 됩니다.
+                else
+                    remainingC += (coolness[nx][ny] - coolness[x][y]) / 4;
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                curCool[i][j] += nextCool[i][j];
-                nextCool[i][j] = 0;
-            }
-        }
+        return remainingC;
     }
-
-    static void makeHot() {
-        for (int i = 0; i < n - 1; i++) {
-            curCool[i][0]--; // 0,0 1,0 2,0 // 0,0에서 밑으로
-            if (curCool[i][0] < 0) curCool[i][0] = 0;
-
-            curCool[0][n - i - 1]--; // 0, n-1, n, n-2 // 0, n-1에서 왼쪽으로
-            if (curCool[0][n - i - 1] < 0) curCool[0][n - i - 1] = 0;
-
-            curCool[n - 1][i]--; // n-1, 0, n-1, 1  // n-1,0에서 오른쪽으로
-            if (curCool[n - 1][i] < 0) curCool[n - 1][i] = 0;
-
-            curCool[n - 1 - i][n - 1]--; // n-1, n-1에서 위쪽으로
-            if (curCool[n - 1 - i][n - 1] < 0) curCool[n - 1 - i][n - 1] = 0;
-        }
+    
+    // 시원함이 mix됩니다.
+    public static void mix() {
+        // temp 배열을 초기화 해줍니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                temp[i][j] = 0;
+    
+        // 각 칸마다 시원함이 mix된 이후의 결과를 받아옵니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++) 
+                temp[i][j] = getMixedCoolness(i, j);
+        
+        // temp 값을 coolness 배열에 다시 옮겨줍니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                coolness[i][j] = temp[i][j];
     }
-    static boolean isCool() {
-        for (int[] office : offices) {
-            if (curCool[office[0]][office[1]] < k) return false;
-        }
+    
+    // 외벽에 해당하는 칸인지를 판단합니다.
+    public static boolean isOuterWall(int x, int y) {
+        return x == 0 || x == n - 1 || y == 0 || y == n - 1;
+    }
+    
+    // 외벽과 인접한 칸 중
+    // 시원함이 있는 곳에 대해서만
+    // 1만큼 시원함을 하락시킵니다.
+    public static void drop() {
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                if(isOuterWall(i, j) && coolness[i][j] > 0)
+                    coolness[i][j]--;
+    }
+    
+    // 시원함이 생기는 과정을 반복합니다.
+    public static void simulate() {
+        // Step1. 에어컨에서 시원함을 발산합니다.
+        blow();
+    
+        // Step2. 시원함이 mix됩니다.
+        mix();
+    
+        // Step3. 외벽과 인접한 칸의 시원함이 떨어집니다.
+        drop();
+    
+        // 시간이 1분씩 증가합니다.
+        elapsedTime++;
+    }
+    
+    // 종료해야 할 순간인지를 판단합니다.
+    // 모든 사무실의 시원함의 정도가 k 이상이거나,
+    // 흐른 시간이 100분을 넘게 되는지를 살펴봅니다.
+    public static boolean end() {
+        // 100분이 넘게 되면 종료해야 합니다.
+        if(elapsedTime > 100)
+            return true;
+        
+        // 사무실 중에
+        // 시원함의 정도가 k 미만인 곳이
+        // 단 하나라도 있으면, 아직 끝내면 안됩니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                if(grid[i][j] == OFFICE && 
+                   coolness[i][j] < k)
+                   return false;
+    
+        // 모두 k 이상이므로, 종료해야 합니다.
         return true;
     }
 
-    static void input() throws IOException {
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+        k = sc.nextInt();
 
-        curCool = new int[n][n];
-        nextCool = new int[n][n];
-        wall = new boolean[4][n][n];
-        offices = new ArrayList<>();
-        aircons = new ArrayList<>();
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
 
-        que = new ArrayDeque<>();
-        nextPos = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                int value = Integer.parseInt(st.nextToken());
-                if (value == 0) continue;
-                if (value == 1) {
-                    offices.add(new int[] {i, j});
-                } else {
-                    aircons.add(new int[] {i, j, value - 2});
-                }
-            }
+        for(int i = 0; i < m; i++) {
+            int bx = sc.nextInt();
+            int by = sc.nextInt();
+            int bdir = sc.nextInt();
+            bx--; by--;
+            // 현재 위치 (bx, by)에서
+            // bdir 방향으로 나아가려고 했을 때
+            // 벽이 있음을 표시해줍니다.
+            block[bx][by][bdir] = true;
+
+            int nx = bx + dx[bdir];
+            int ny = by + dy[bdir];
+            // 격자를 벗어나지 않는 칸과 벽을 사이에 두고 있다면,
+            // 해당 칸에서 반대 방향(3-bdir)으로 진입하려고 할 때도
+            // 벽이 있음을 표시해줍니다.
+            if(inRange(nx, ny))
+                block[nx][ny][3 - bdir] = true;
         }
 
-        // 왼쪽, 위, 오른쪽, 아래
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int y = Integer.parseInt(st.nextToken()) - 1;
-            int x = Integer.parseInt(st.nextToken()) - 1;
-            int dir = Integer.parseInt(st.nextToken());
-            if (dir == 0) { // 위쪽에 벽
-                wall[1][y][x] = true; // 위 방향 기준
-                if (y - 1 != n) wall[3][y - 1][x] = true; // 아래 방향 기준
-            } else { // 왼쪽에 벽
-                wall[0][y][x] = true;
-                if (x - 1 != n) wall[2][y][x - 1] = true; // 오른쪽 방향 기준
-            }
-        }
+        // 종료조건이 만족되기 전까지
+        // 계속 시뮬레이션을 반복합니다.
+        while(!end())
+            simulate();
+
+        // 출력:
+        if(elapsedTime <= 100)
+            System.out.print(elapsedTime);
+        else
+            System.out.print(-1);
     }
 }
